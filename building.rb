27cd5e1c1e4666738a3738f21903d2b42971a169
Cyberia 2045 <sagansociety86@gmail.com>
2017-05-09 14:29:35 -0400

    closes #3

--- building.rb
+++ building.rb
@@ -4,5 +4,23 @@ class Building
 		@floors = 10
 		@units = 50
 	end
+end
 
+class TopFloors
+
+	def initialize
+		@leased = true
+		@HOA = cost * 0.2
+		super()
+	end
+
+	def Floor10
+		@cost = 2000
+		@units = 5
+	end
+
+	def Floor9
+		@cost = 1800
+		@units = 5
+	end
 end
